219eb22ca82e14be5c6ebab5307b2d190e81bdfb
Cayman <caymannava@gmail.com>
2015-10-25 18:37:23 -0700

    add some light documentation

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 Simple virtual file format based on [vinyl](https://github.com/gulpjs/vinyl).
 
+A vfile, or virtual file, provides a file-like interface for accessing the
+contents of arbitrary data as a stream of element type `(unsigned byte 8)`.
+
+The major attributes of a vfile are:
+ - its path metadata
+ - its contents, the underlying data
+
+To access the contents of a vfile, the vfile should be opened, and read from
+the returned stream.
+
 ## License
 
 MIT
--- src/interface.lisp
+++ src/interface.lisp
@@ -1,16 +1,35 @@
 (cl:in-package #:vfile)
 
+;;; A vfile, or virtual file, is a file metadata object that provides a
+;;; file-like interface to some file contents as a stream of
+;;; `(unsigned byte 8)` elements.
+
+;;; A vfile consists of:
+;;;  - cwd - a current working directory
+;;;  - base - a path used for relative pathing
+;;;  - path - the full path string of the file
+;;;  - contents - the file contents
+
+;;; cwd
+
 (defgeneric vfile-cwd (vfile))
 
 (defgeneric (setf vfile-cwd) (vfile cwd))
+
+;;; base
 
 (defgeneric vfile-base (vfile))
 
 (defgeneric (setf vfile-base) (vfile base))
 
-(defgeneric vfile-contents (vfile))
+;;; path accessors
 
-(defgeneric (setf vfile-contents) (vfile contents))
+;;; These functions are based off of corresponding functions in the
+;;; path-string package, which is based off of the node.js path library.
+
+(defgeneric vfile-path (vfile))
+
+(defgeneric (setf vfile-path) (vfile path))
 
 (defgeneric vfile-dirname (vfile))
 
@@ -24,13 +43,19 @@
 
 (defgeneric (setf vfile-extname) (vfile extname))
 
-(defgeneric vfile-path (vfile))
-
-(defgeneric (setf vfile-path) (vfile path))
-
 (defgeneric vfile-relative (vfile))
 
-(defgeneric vfile-open (vfile &key direction))
+;;; contents
+
+(defgeneric vfile-contents (vfile))
+
+(defgeneric (setf vfile-contents) (vfile contents))
+
+;;; open
+
+;;; vfile-open returns a stream representation of VFILE in the same style as
+;;; cl:open does with pathnames.
+(defgeneric vfile-open (vfile &key direction &allow-other-keys))
 
 (defgeneric contents-input-stream (contents))
 
